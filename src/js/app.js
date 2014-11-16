ebf3858e8039fcfeba176dd7ac11ebc5dac89b75
Ivar Nilsen <post@ivarnilsen.com>
2014-11-16 17:50:17 +0100

    Topcoat and css

--- src/js/app.js
+++ src/js/app.js
@@ -7,11 +7,11 @@ db.changes({
     live: true
 }).on('change', getItems);
 
+getItems();
 sync();
 
 function sync() {
     var opts = { live: true };
-    db.replicate.to(remoteCouch, opts, syncError);
     db.replicate.from(remoteCouch, opts, syncError);
 }
 
@@ -35,11 +35,33 @@ function createList(items) {
     var ul = document.createElement('ul');
     items.forEach(function(item) {
         var text = item.doc.text;
-        var li = document.createElement('li');
-        li.innerHTML = text;
-        ul.appendChild(li);
+        var item = createListItem(text);
+        ul.appendChild(item);
     });
     return ul;
+}
+
+function createListItem(text) {
+    var label = document.createElement('label');
+    label.setAttribute('class', 'topcoat-checkbox');
+
+    var span = document.createElement('span');
+    span.innerHTML = text;
+
+    var input = document.createElement('input');
+    input.setAttribute('type', 'checkbox');
+
+    var div = document.createElement('div');
+    div.setAttribute('class', 'topcoat-checkbox__checkmark');
+
+    label.appendChild(input);
+    label.appendChild(div);
+    label.appendChild(span);
+
+    var li = document.createElement('li');
+    li.appendChild(label);
+
+    return li;
 }
 
 function handleAddEvent(event) {
